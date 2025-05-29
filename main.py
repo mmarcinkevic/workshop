a830164c98e1654d62f82c59fe423fb8d7027326
mmarcinkevic <marcinkevic.martin@gmail.com>
2025-05-29 15:14:32 +0300

    check

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-def say_hello():
-    print("Hello")
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os
+from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    say_hello()
+load_dotenv()
+
+url = "https://vinted.atlassian.net/rest/api/3/issue/IT-55555"
+name = os.environ.get("JIRA_ACCOUNT_EMAIL")
+token = os.environ.get("JIRA_ACCOUNT_TOKEN")
+
+auth = HTTPBasicAuth(name, token)
+
+headers = {
+  "Accept": "application/json"
+}
+
+response = requests.request(
+   "GET",
+   url,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
